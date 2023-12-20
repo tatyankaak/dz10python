4c1f78b489c3555a77d6182fc28b187a5ce72e1c
tatyankaak <138118882+tatyankaak@users.noreply.github.com>
2023-12-20 20:24:08 +0300

    Update Program.cs

--- Desktop/Новая папка/Program.cs
+++ Desktop/Новая папка/Program.cs
@@ -1,1 +1,15 @@
+import random
+import pandas as pd
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
 
+unique_values = data['whoAmI'].unique()
+one_hot_data = pd.DataFrame()
+
+for value in unique_values:
+    one_hot_data[value] = (data['whoAmI'] == value).astype(int)
+
+print(one_hot_data.head())
